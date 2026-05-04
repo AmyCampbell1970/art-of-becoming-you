1a34443a76198969c1cf344a4a1432db25084f23
AmyCampbell1970 <theartofbecomingyou@gmail.com>
2026-05-04 15:08:33 -0500

    Add robots.txt pointing to sitemap

---
+++ src/app/robots.ts
@@ -0,0 +1,11 @@
+import { MetadataRoute } from "next";
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: "*",
+      allow: "/",
+    },
+    sitemap: "https://art-of-becoming-you.vercel.app/sitemap.xml",
+  };
+}
